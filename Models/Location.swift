bb938b84dfaa34f8d505005bb47a87a763755754
Fatih Eren Özçelik <f.eren.ozcelik@gmail.com>
2024-03-11 09:46:40 +0300

    Project structure, MVVM & Assets

---
+++ Models/Location.swift
@@ -0,0 +1,12 @@
+//
+//  Location.swift
+//  TripMap
+//
+//  Created by Fatih Eren Ozcelik on 11.03.2024.
+//
+
+import Foundation
+
+struct Location {
+    
+}
---
+++ TripMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripMapApp.swift
+//  TripMap
+//
+//  Created by Fatih Eren Ozcelik on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TripMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
